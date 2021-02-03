a88be6143ea208bec78aee407bebbb234a29c53a
GabrielVander <gabsvander@hotmail.com>
2021-02-03 18:22:14 -0300

    DB connection

--- dist-server/app.js
+++ dist-server/app.js
@@ -16,16 +16,21 @@ var _http = require("http");
 
 var _env = require("./env");
 
+var _cors = _interopRequireDefault(require("cors"));
+
+var _connection = require("./repository/connection");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
+app.use((0, _cors["default"])());
 app.use((0, _morgan["default"])('dev'));
 app.use(_express["default"].json());
 app.use(_express["default"].urlencoded({
   extended: false
 }));
 app.use((0, _cookieParser["default"])());
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'public')));
+(0, _connection.initializeMongoose)();
 app.use('/', _routes["default"]);
 app.use('/users', _users["default"]);
 var server = (0, _http.createServer)(app);
--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes";
 import usersRouter from "./routes/users";
 import {createServer} from "http";
 import {port} from "./env";
+import cors from "cors";
+import {initializeMongoose} from "./repository/connection";
 
 const app = express();
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+initializeMongoose();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
